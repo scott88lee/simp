11e4b3e16bd4234fca7c5d0e67acdaea50f7ff75
Scott Lee <36765819+scott88lee@users.noreply.github.com>
2022-08-01 01:57:16 +0800

    debug method

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 const notFound = (req, res) => {
     let resource = req.originalUrl || req.url;
+    let method= req.method;
     res.status(404).send(
-        '<h1>404 Not Found</h1>' +  resource
+        '<h1>404 Not Found</h1>' +  
+        method + ": " + resource
     );
 }
 
